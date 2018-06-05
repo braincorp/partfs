38389e8ea6bf17bfec18cf7062e021fa3879d431
John Tyner <tyner@braincorporation.com>
2018-06-05 12:49:22 -0700

    add example commands

--- partfs.c
+++ partfs.c
@@ -9,6 +9,39 @@
  *
  * the mounted directory presents the partitions as files, allowing
  * mkfs.* to be used to create file systems on the partitions
+ *
+ * to create a disk image with a single bootable
+ * partition with an ext4 file system:
+ *
+ * $ dd if=/dev/zero of=disk.image bs=1M count=4
+ * 4+0 records in
+ * 4+0 records out
+ * 4194304 bytes (4.2 MB, 4.0 MiB) copied, 0.00470867 s, 891 MB/s
+ * $ parted --script disk.image \
+ *     mktable msdos mkpart primary 2048s 100% set 1 boot on
+ * $ mkdir mntdir
+ * $ partfs -o dev=disk.image mntdir
+ * $ mkfs.ext4 mntdir/part_0
+ * mke2fs 1.42.13 (17-May-2015)
+ * Creating filesystem with 3072 1k blocks and 768 inodes
+ *
+ * Allocating group tables: done
+ * Writing inode tables: done
+ * Creating journal (1024 blocks): done
+ * Writing superblocks and filesystem accounting information: done
+ *
+ * $ fusermount -u mntdir
+ *
+ * to verify that the partition and file
+ * system were successfully created:
+ *
+ * $ sudo kpartx -a -v disk.image
+ * add map loop0p1 (253:0): 0 6144 linear 7:0 2048
+ * $ sudo fsck.ext4 /dev/mapper/loop0p1
+ * e2fsck 1.42.13 (17-May-2015)
+ * /dev/mapper/loop0p1: clean, 11/768 files, 1150/3072 blocks
+ * $ sudo kpartx -d disk.image
+ * loop deleted : /dev/loop0
  */
 
 #define FUSE_USE_VERSION        26
